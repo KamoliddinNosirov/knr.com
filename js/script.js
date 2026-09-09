ecd13b36376e4635c0bb743788e23c08a93bf1e5
KamoliddinNosirov <kamoliddin3425@gmail.com>
2026-09-09 06:56:39 +0500

    About page yozildi! Nav bo'limi scroll bo'lganda qatirildi. Hozircha muammolar yo'q

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const bgheader = () => {
+  const navTag = document.querySelector('.navbg')
+  if (window.scrollY > 20) {
+    navTag.classList.add("bgheader")
+  } else {
+    navTag.classList.remove("bgheader")
+  }
+}
+
+window.addEventListener("scroll", bgheader)
---
+++ js/typid.js
@@ -0,0 +1,7 @@
+// typing animation
+var typed = new Typed(".typing", {
+    strings:["Front End Developer", "Freelance", "Web Developer", "YouTuber"],
+    typeSpeed:200,
+    BackSpeed:60,
+    loop:true
+})
