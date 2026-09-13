baf96847802cfbd4591899ff4f389e3af1f4f7fa
KamoliddinNosirov <kamoliddin3425@gmail.com>
2026-09-13 22:27:19 +0500

    home, about, talks, blog, page full. Database add

--- js/script.js
+++ js/script.js
@@ -11,6 +11,15 @@ window.addEventListener("scroll", bgheader)
 
 // Brief user database
 
-const userDatabase = [
-  
-]
+// const userDatabase = []
+
+// ------ Blog posts database ---------
+
+// const postsDatabase = [
+//   {
+//     id: 0,
+//     title: "20 yoshgacha 20 urinish",
+//     date: "17 December, 2025",
+//   }
+// ]
+
