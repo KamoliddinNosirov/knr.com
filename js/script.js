c8a00c1c945e26ee3c5ef923b783a3c33a1d9bcd
KamoliddinNosirov <kamoliddin3425@gmail.com>
2026-09-15 07:02:25 +0500

    home page media add. ++tabled

--- js/script.js
+++ js/script.js
@@ -23,3 +23,19 @@ window.addEventListener("scroll", bgheader)
 //   }
 // ]
 
+// --------- Nav menu active --------
+const navEl = document.querySelector("nav")
+const menuBtn = document.querySelector("nav button")
+
+menuBtn.addEventListener("click", ()=>{
+  navEl.classList.toggle("active")
+  const iTag = menuBtn.querySelector("i")
+  if(navEl.classList != "active"){
+    iTag.classList.add("fa-bars")
+    iTag.classList.remove("fa-x")
+  }else{
+    iTag.classList.remove("fa-bars")
+    iTag.classList.add("fa-x")
+  }
+  
+})
